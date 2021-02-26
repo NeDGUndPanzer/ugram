cc5375ca5332388ba476e980e7ee199ee5138536
Nery Eduardo Herrera <tesco.herrera.edu@gmail.com>
2021-02-26 15:38:36 -0600

    [ADD] modulo de singup

--- ulgram/src/app/app.module.ts
+++ ulgram/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
