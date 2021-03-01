1e1af99c77d5693d59bc24814a7d8affdc8e25a7
Nery Eduardo Herrera <tesco.herrera.edu@gmail.com>
2021-03-01 16:55:18 -0600

    [fixed] error en componente front

--- Backend/Nodejs/src/routes/creds.js
+++ Backend/Nodejs/src/routes/creds.js
@@ -1,8 +1,8 @@
 let aws_keys = {
     s3: {
         region: 'us-east-2',
-        accessKeyId: "AKIAQGEZTINBG6FXCMHG",
-        secretAccessKey: "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",
+        accessKeyId: "",
+        secretAccessKey: "",
     }
 }
 module.exports = aws_keys
