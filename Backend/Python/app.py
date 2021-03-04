277377ad7bb21fb37e18466470089c167d39b2ea
bitochepe <juanjohr97@gmail.com>
2021-03-04 03:13:03 -0600

    Creadas todas las rutas para el servidor python

--- Backend/Python/app.py
+++ Backend/Python/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import json
 import hashlib
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import base64
 import uuid
 
@@ -67,10 +68,7 @@ def registrarUsuario():
             return jsonify(response)
         except KeyError:
             try:
-                imagen = base64.b64decode(photo)
-                nombre = 'Fotos_Perfil/' + str(uuid.uuid4()) + '.jpg'
-                s3.Bucket('practica1-g21-imagenes').put_object(Key=nombre,Body=imagen, ContentType="image", ACL='public-read')
-                
+                nombre = subirImagen(photo)
                 userstable.put_item(
                 Item={
                     'username':username,
@@ -89,12 +87,12 @@ def registrarUsuario():
                 Item={
                     'username':username,
                     'albumName':'Perfil',
-                    'photo': urlbucket+nombre
+                    'imgurl': urlbucket+nombre
                 })
                 response = {"status":'200', 'creado' : 'true'}
                 return jsonify(response)
             except:
-                response = {"status":'200', 'creado' : 'false', 'msg':'Ocurrio un error'+str(Exception)}
+                response = {"status":'200', 'creado' : 'false', 'msg':'Ocurrio un error'}
                 return jsonify(response)
 
 @app.route('/update',methods = ['PUT'])
@@ -144,6 +142,134 @@ def eliminarUser():
         except:
             response = {"status":'200', 'eliminado' : 'false', 'msg':'Ocurrio un error'}
             return jsonify(response)
+
+@app.route('/getuserdata', methods = ['GET'])
+def getUsuario():
+     if request.method == 'GET':
+        username = request.json['username']
+        try:
+            res = userstable.get_item(
+            Key = {
+                "username":username
+            })
+            item = res['Item']
+            response = {'username': item['username'], 'name': item['fullname'],'foto': item['photo']}
+            return jsonify(response)
+        except:
+            response = {"status":'200', 'get' : 'false', 'msg':'Ocurrio un error'}
+            return jsonify(response)
+
+@app.route('/getuseralbums', methods = ['GET'])
+def getuserAlbums():
+     if request.method == 'GET':
+        username = request.json['username']
+        try:
+            res = albumtable.query(
+                KeyConditionExpression=Key('username').eq(username))
+            item = res['Items']
+            datos = ""
+            for x in item:
+                datos += x['albumName']+";"
+            
+            return datos[0:len(datos)-1]
+        except:      
+            response = {"status":'200', 'get' : 'false', 'msg':'Ocurrio un error'}
+            return jsonify(response)
+
+@app.route('/addAlbum',methods = ['POST'])
+def addAlbum():
+    if request.method == 'POST':
+        username = request.json['username']
+        albumname = request.json['album'] 
+        try:
+            albumtable.put_item(
+            Item={
+                'username':username,
+                'albumName':albumname
+            })
+            response = {"status":'200', 'agregado' : 'true'}
+            return jsonify(response)
+        except:
+            response = {"status":'200', 'agregado' : 'false', 'msg':'Ocurrio un error'}
+            return jsonify(response)
+
+@app.route('/addFoto',methods = ['POST'])
+def addFoto():
+    if request.method == 'POST':
+        username = request.json['username']
+        albumname = request.json['albumName'] 
+        foto = request.json['foto']
+        try:
+            nombre = subirImagen(foto)
+            fotostable.put_item(
+            Item={
+                'username':username,
+                'albumName':albumname,
+                'imgurl' : urlbucket+nombre
+            })
+            response = {"status":'200', 'agregado' : 'true'}
+            return jsonify(response)
+        except:
+            response = {"status":'200', 'agregado' : 'false', 'msg':'Ocurrio un error'}
+            return jsonify(response)
+
+@app.route('/deleteFoto',methods = ['DELETE'])
+def deleteFoto():
+    if request.method == 'DELETE':
+        foto = request.json['foto']
+        try:
+            fotostable.delete_item(
+            Key={
+                'imgurl': foto
+            })
+            response = {"status":'200', 'eliminado' : 'true'}
+            return jsonify(response)
+        except:
+            response = {"status":'200', 'eliminado' : 'false', 'msg':'Ocurrio un error'}
+            return jsonify(response)
+
+@app.route('/deleteAlbum',methods = ['DELETE'])
+def deleteAlbum():
+    if request.method == 'DELETE':
+        album = request.json['albumName']
+        username = request.json['username']
+        try:
+            res = fotostable.scan(
+            FilterExpression=Attr('username').eq(username) & Attr('albumName').eq(album))
+            item = res['Items']
+            if not item:
+                datos = []
+            else:
+                datos =[]
+                for x in item:
+                    datos.append(x['imgurl'])
+                for y in datos:
+                    borrarFoto(y)
+            albumtable.delete_item(
+            Key={
+                'username' : username,
+                'albumName' : album
+            })
+            response = {"status":'200', 'eliminado' : 'true'}
+            return jsonify(response)
+        except:
+            response = {"status":'200', 'eliminado' : 'false', 'msg':'Ocurrio un error'}
+            return jsonify(response)
+
+def subirImagen(photo):
+    imagen = base64.b64decode(photo)
+    nombre = 'Fotos_Perfil/' + str(uuid.uuid4()) + '.jpg'
+    s3.Bucket('practica1-g21-imagenes').put_object(Key=nombre,Body=imagen, ContentType="image", ACL='public-read')
+    return nombre
+
+def borrarFoto(llave):
+    try:
+        fotostable.delete_item(
+        Key={
+            'imgurl': llave
+        })
+    except:
+        return
 
 if __name__ == '__main__':
     app.run()
