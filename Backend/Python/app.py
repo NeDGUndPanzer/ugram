31e562a9a83b5542889ea8d62da4b43c13c2ad8c
bitochepe <juanjohr97@gmail.com>
2021-03-06 00:20:40 -0600

    Arreglo de rutas registro y credenciales

--- Backend/Python/app.py
+++ Backend/Python/app.py
@@ -54,7 +54,7 @@ def registrarUsuario():
         username = request.json['username']
         passwd = request.json['password']
         encripted = hashlib.md5(passwd.encode())
-        fullname = request.json['fullname']
+        fullname = request.json['name']
         photo = request.json['foto']
 
         try:
@@ -311,14 +311,14 @@ def getuserfotos():
             return jsonify(response)
 def subirImagen(photo):
     imagen = base64.b64decode(photo)
-    nombre = 'Fotos_Publicadas/' + str(uuid.uuid4()) + '.jpg'
-    s3.Bucket('practica1-g21-imagenes').put_object(Key=nombre,Body=imagen, ContentType="image", ACL='public-read')
+    nombre =  str(uuid.uuid4())
+    s3.Bucket('practica1-g21-imagenes').put_object(Key='Fotos_Publicadas/' +nombre + '.jpg',Body=imagen, ContentType="image", ACL='public-read')
     return nombre
 
 def subirImagenPerfil(photo):
     imagen = base64.b64decode(photo)
-    nombre = 'Fotos_Perfil/' + str(uuid.uuid4()) + '.jpg'
-    s3.Bucket('practica1-g21-imagenes').put_object(Key=nombre,Body=imagen, ContentType="image", ACL='public-read')
+    nombre = str(uuid.uuid4()) 
+    s3.Bucket('practica1-g21-imagenes').put_object(Key='Fotos_Perfil/' + nombre+ '.jpg',Body=imagen, ContentType="image", ACL='public-read')
     return nombre
 
 def borrarFoto(llave):
@@ -331,4 +331,4 @@ def borrarFoto(llave):
         return
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
