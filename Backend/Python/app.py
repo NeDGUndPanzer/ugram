f9492598f0aecc675147ce433a00118cdd522b12
bitochepe <juanjohr97@gmail.com>
2021-03-08 21:08:05 -0600

    Posible final python

--- Backend/Python/app.py
+++ Backend/Python/app.py
@@ -54,7 +54,7 @@ def registrarUsuario():
         username = request.json['username']
         passwd = request.json['password']
         encripted = hashlib.md5(passwd.encode())
-        fullname = request.json['name']
+        fullname = request.json['nombre']
         photo = request.json['foto']
 
         try:
@@ -100,8 +100,8 @@ def updateUser():
     if request.method == 'PUT':
         username = request.json['username']
         usernameE = request.json['newuser']
-        fullname = request.json['fullname']
-        photo = request.json['photo']
+        fullname = request.json['nombre']
+        photo = request.json['foto']
 
         try:
             res = userstable.get_item(
@@ -313,13 +313,13 @@ def subirImagen(photo):
     imagen = base64.b64decode(photo)
     nombre =  str(uuid.uuid4())
     s3.Bucket('practica1-g21-imagenes').put_object(Key='Fotos_Publicadas/' +nombre + '.jpg',Body=imagen, ContentType="image", ACL='public-read')
-    return nombre
+    return jsonify( {"idfoto" : nombre})
 
 def subirImagenPerfil(photo):
     imagen = base64.b64decode(photo)
     nombre = str(uuid.uuid4()) 
     s3.Bucket('practica1-g21-imagenes').put_object(Key='Fotos_Perfil/' + nombre+ '.jpg',Body=imagen, ContentType="image", ACL='public-read')
-    return nombre
+    return jsonify( {"idfoto" : nombre})
 
 def borrarFoto(llave):
     try:
