a63a01b9be96db9b5976310a6fa941fbbe40ea14
bitochepe <juanjohr97@gmail.com>
2021-04-08 18:02:43 -0600

    Ruta de translate creada

--- Backend/Nodejs/src/routes/creds.js
+++ Backend/Nodejs/src/routes/creds.js
@@ -1,18 +1,23 @@
 let aws_keys = {
     s3: {
-        region: 'us-east-2',
-        accessKeyId: "AKIAQGEZTINBDMUDZ3E4",
-        secretAccessKey: "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",
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: "",
     },
     dynamodb: {
-        region: 'us-east-2',
-        accessKeyId: "AKIAVEMKH4ZBHWPR4M7A",
-        secretAccessKey: "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"
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: ""
     },
     rekognition: {
-        region: 'us-east-2',
-        accessKeyId: "AKIAQGEZTINBFKPAWIEG",
-        secretAccessKey: "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" 
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: "" 
+    },
+    translate: {
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: "" 
     }
 }
 module.exports = aws_keys
--- Backend/Nodejs/src/routes/index.js
+++ Backend/Nodejs/src/routes/index.js
@@ -19,6 +19,7 @@ const aws_keys = require('./creds');
 const s3 = new AWS.S3(aws_keys.s3);
 const ddb = new AWS.DynamoDB(aws_keys.dynamodb);
 const rek = new AWS.Rekognition(aws_keys.rekognition);
+const awstranslate = new AWS.Translate(aws_keys.translate);
 
 router.get('/hola', 
     (req,res) => res.json
@@ -564,6 +565,28 @@ router.post('/getLabels', async (req, res) => {
     }
   });
 
+});
+
+//Translates text from the received json and return the translated text
+
+router.post('/traducir', (req, res) => {
+  let description = req.body.description
+  let toleng = req.body.toleng
+
+  let params = {
+    SourceLanguageCode: 'auto',
+    TargetLanguageCode: toleng,
+    Text: description || 'Error al traducir'
+  };
+  awstranslate.translateText(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.send({ error: err })
+    } else {
+      console.log(data);
+      res.send({ translated: data })
+    }
+  });
 });
 
 module.exports = router;
