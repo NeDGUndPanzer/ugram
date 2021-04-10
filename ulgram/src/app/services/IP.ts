0c2871be7788dc4e44320a36fc5035c0cb16f9de
Nery Eduardo Herrera <tesco.herrera.edu@gmail.com>
2021-04-09 19:19:57 -0600

    [add] nueva ip

--- ulgram/src/app/services/IP.ts
+++ ulgram/src/app/services/IP.ts
@@ -1,4 +1,4 @@
 //var ip = `http://loadbalancerp1g21-1022405401.us-east-2.elb.amazonaws.com/`;
-var ip = `http://localhost:3000/`;
+var ip = `http://3.143.205.81:3000/`;
 // http://loadbalancerp1g21-1022405401.us-east-2.elb.amazonaws.com/
 export default ip;
